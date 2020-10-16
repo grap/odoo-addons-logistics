6d555d235d72da7ff9e93aa5ed146cefb955cb18
lucien <lucien@hashbang.fr>
2020-10-16 16:28:44 +0200

    Init module Joint Buying Purchase

---
+++ joint_buying_purchase/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2020-Today: HASHBANG (https://hashbang.coop)
+# @author: HAshbang (https://twitter.com/HashBangCoop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Joint Buying - Purchase",
+    'summary': "A module that allows activities to make grouped orders.",
+    'category': "GRAP - Logistics",
+    'author': "Hashbang",
+    'website': "https://hashbang.coop",
+    "license": "AGPL-3",
+    'depends': ["web_one2many_kanban", "purchase", "joint_buying_product"],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/view_tour_template.xml',
+        'views/view_res_partner.xml',
+        'views/view_tour.xml',
+        'views/view_joint_buying_purchase_order.xml',
+        'views/menu.xml',
+    ],
+    "installable": True,
+}
---
+++ joint_buying_purchase/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import res_partner, joint_tour, joint_purchase, joint_tour_template
