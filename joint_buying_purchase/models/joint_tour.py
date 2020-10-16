cba3c6b8153452520246dd565014e6b8ab4fe814
lucien <lucien@hashbang.fr>
2020-10-16 16:30:18 +0200

    Add model / view - Tour

---
+++ joint_buying_purchase/models/joint_tour.py
@@ -0,0 +1,15 @@
+from odoo import fields, models
+
+
+class JointBuyingTour(models.Model):
+    _name = "joint.buying.tour"
+    _description = "Joint buying tour"
+    _rec_name = "date"
+
+    date = fields.Date(primary=True)
+    tour_template_id = fields.Many2one("joint.buying.tour.template")
+    joint_buying_purchase_ids = fields.One2many(
+        'joint.buying.purchase.order',
+        inverse_name="tour_id",
+        string="Joint buying purchase orders peer supplier"
+    )
