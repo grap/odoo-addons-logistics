72646331cca7abb24fc850a9701b17cd8ecd7068
lucien <lucien@hashbang.fr>
2020-10-19 14:16:43 +0200

    Update : Correction review Sylvain

--- joint_buying_purchase/models/joint_buying_tour.py
+++ joint_buying_purchase/models/joint_buying_tour.py
@@ -6,8 +6,10 @@ class JointBuyingTour(models.Model):
     _description = "Joint buying tour"
     _rec_name = "date"
 
-    date = fields.Date(primary=True)
-    tour_template_id = fields.Many2one("joint.buying.tour.template")
+    date = fields.Date(primary=True, required=True)
+    tour_template_id = fields.Many2one(
+        "joint.buying.tour.template", required=True, ondelete='set null'
+    )
     joint_buying_purchase_ids = fields.One2many(
         'joint.buying.purchase.order',
         inverse_name="tour_id",
