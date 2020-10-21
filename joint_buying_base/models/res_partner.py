4083f56ac097ee503f3f17c54c2c6ae60ff2faa8
lucien <lucien@hashbang.fr>
2020-10-21 12:06:12 +0200

    Add demo - fix version in joint_buying_purchase manifest file

--- joint_buying_base/models/res_partner.py
+++ joint_buying_base/models/res_partner.py
@@ -15,3 +15,21 @@ class ResPartner(models.Model):
     is_joint_buying_supplier = fields.Boolean(
         default=False, string="Is a supplier for joint buying"
     )
+
+    # Supplier
+    activity_id = fields.Many2one(
+        "res.partner",
+        string="The activity that manages my stocks",
+        domain=[("is_joint_buying", "=", True), ("is_joint_buying_supplier", "=", True)]
+    )
+
+    # Customer
+    supplier_ids = fields.One2many(
+        "res.partner",
+        inverse_name="activity_id",
+        strings=(
+            "Suppliers to manage, if there are one or more company,"
+            " then this is a pivot activity"
+        ),
+        domain=[("is_joint_buying", "=", True), ("is_joint_buying_supplier", "=", True)]
+    )
