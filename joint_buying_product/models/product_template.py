a4c7225256c11464a136a6d650d105e01fc5e500
lucien <lucien@hashbang.fr>
2020-10-22 18:05:04 +0200

    Remove fields in product_template model and update override view product_template_form

--- joint_buying_product/models/product_template.py
+++ joint_buying_product/models/product_template.py
@@ -2,14 +2,9 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import models
 
 
 class ProductTemplate(models.Model):
     _inherit = ["product.template", "joint.buying.mixin"]
     _name = "product.template"
-
-    joint_buying_seller_ids = fields.One2many("product.supplierinfo", "product_tmpl_id")
-    joint_buying_variant_seller_ids = fields.One2many(
-        "product.supplierinfo", "product_tmpl_id"
-    )
