3983963d80464c5689489736de951c64dc0afa23
lucien <lucien@hashbang.fr>
2020-10-26 10:48:07 +0100

    Add color indication to show the quantity state of each line in Order

--- joint_buying_purchase/models/joint_buying_purchase_order_line.py
+++ joint_buying_purchase/models/joint_buying_purchase_order_line.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 from odoo.addons import decimal_precision as dp
 
@@ -23,3 +23,32 @@ class JointBuyingPurchaseOrderLine(models.Model):
         digits=dp.get_precision("Product Unit of Measure"),
         required=True,
     )
+    quantity_validation = fields.Integer(compute="_compute_quantity_validation")
+
+    _sql_constraints = [
+        ("product_id_unique", "unique(product_id)", "It's on product peer line")
+    ]
+
+    @api.onchange("quantity")
+    def _compute_quantity_validation(self):
+        """
+        Check if the quantity of lines not exceeded the
+        quantity allowed for the product or is supperior
+        to minimum quantity of supplier.
+        """
+        if not self.product_id:
+            return
+
+        supplier_id = self.product_id.seller_ids.search(
+            [("name", "=", self.order_id.supplier_id.id)]
+        )
+        # Critical case: If the current product have multiple
+        # seller_ids with the same supplier
+        quantity_allowed = supplier_id.max_qty
+        minimum_quantity = supplier_id.min_qty
+        if self.quantity > quantity_allowed:
+            self.update({"quantity_validation": 2})
+        elif self.quantity < minimum_quantity:
+            self.update({"quantity_validation": 1})
+        else:
+            self.update({"quantity_validation": 0})
