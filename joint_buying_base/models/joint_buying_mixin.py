08b38e3187ec1f54d53fda1755d04c3d17fc89ce
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-03-15 13:16:02 +0100

    [FIX][WIP] joint_buying_base, trying to fix bug

--- joint_buying_base/models/joint_buying_mixin.py
+++ joint_buying_base/models/joint_buying_mixin.py
@@ -45,27 +45,79 @@ class JointBuyingMixin(models.AbstractModel):
                 vals.update({"joint_buying": True, "company_id": False})
         return super().create(vals)
 
-    # Overload the private _search function:
-    # This function is used by the other ORM functions
-    # (name_search, search_read)
     @api.model
-    def _search(
-        self,
-        args,
-        offset=0,
-        limit=None,
-        order=None,
-        count=False,
-        access_rights_uid=None,
-    ):
-        args += [
+    def _join_buying_mixin_add_domain(self, domain):
+        return domain + [
             ("is_joint_buying", "=", bool(self.env.context.get("joint_buying", False)))
         ]
-        return super()._search(
-            args=args,
+
+    @api.model
+    def search(self, domain, offset=0, limit=None, order=None, count=False):
+        if self._name == "res.partner":
+            print("joint_buying_base::search")
+        domain = self._join_buying_mixin_add_domain(domain)
+        return super().search(
+            domain,
             offset=offset,
             limit=limit,
             order=order,
             count=count,
-            access_rights_uid=access_rights_uid,
         )
+
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        if self._name == "res.partner":
+            print("joint_buying_base::name_search")
+        args = self._join_buying_mixin_add_domain(args)
+        return super().name_search(
+            name=name, args=args, operator=operator, limit=limit
+        )
+
+
+
+
+
+    # # Overload the private _search function:
+    # # This function is used by the other ORM functions
+    # # (name_search, search_read)
+    # @api.model
+    # def _search(
+    #     self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None
+    # ):
+    #     if self._name == "res.partner":
+    #         print("joint_buying_base::_search")
+    #     args += [
+    #         ("is_joint_buying", "=", bool(self.env.context.get("joint_buying", False)))
+    #     ]
+    #     return super()._search(
+    #         args,
+    #         offset=offset,
+    #         limit=limit,
+    #         order=order,
+    #         count=count,
+    #         access_rights_uid=access_rights_uid,
+    #     )
+
+    # @api.model
+    # def name_search(self, name='', args=None, operator='ilike', limit=100):
+    #     if self._name == "res.partner":
+    #         print("joint_buying_base::name_search")
+    #     return super().name_search(
+    #         name=name, args=args, operator=operator, limit=limit
+    #     )
+
+    # @api.model
+    # def _name_search(
+    #     self, name='', args=None, operator='ilike', limit=100, name_get_uid=None
+    # ):
+    #     # Overload also _name_search
+    #     # because res.partner._name_search doesn't call super in all
+    #     # cases. (so doesn't call _search)
+    #     if self._name == "res.partner":
+    #         print("joint_buying_base::_name_search")
+    #     args += [
+    #         ("is_joint_buying", "=", bool(self.env.context.get("joint_buying", False)))
+    #     ]
+    #     return super()._name_search(
+    #         name=name, args=args, operator=operator, limit=limit, name_get_uid=name_get_uid
+    #     )
