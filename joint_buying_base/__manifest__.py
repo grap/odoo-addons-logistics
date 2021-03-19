1b73e42693248f8de9166e7aba87cb06bc398d96
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-03-19 21:17:10 +0100

    [FIX] joint_buying_base : depends on name_search_reset_res_partner; [ADD] test regarding search / name_search partners

--- joint_buying_base/__manifest__.py
+++ joint_buying_base/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "GRAP",
     "website": "http://www.grap.coop",
     "license": "AGPL-3",
-    "depends": ["base", "mail"],
+    "depends": ["base", "mail", "name_search_reset_res_partner"],
     "data": [
         "security/ir_module_category.xml",
         "security/res_groups.xml",
