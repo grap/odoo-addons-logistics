fad4ec0597df88b7336d88a863f9c84af387f98d
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-04-20 15:10:56 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup!  [IMP] joint_buying_product

--- joint_buying_product/tests/test_module.py
+++ joint_buying_product/tests/test_module.py
@@ -2,6 +2,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase, at_install, post_install
 
 
@@ -10,21 +11,29 @@ from odoo.tests.common import TransactionCase, at_install, post_install
 class TestModule(TransactionCase):
     def setUp(self):
         super().setUp()
-        self.ProductProduct = self.env["product.product"]
+        self.ProductProduct = self.env["product.product"].with_context(
+            mail_create_nosubscribe=True
+        )
         self.JointBuyingProductProduct = self.env["product.product"].with_context(
-            joint_buying=True
+            mail_create_nosubscribe=True, joint_buying=True
         )
         self.company_ELD = self.env.ref("joint_buying_base.company_ELD")
-        self.category_all = self.env.ref("product.category_all")
+        self.partner_supplier_fumet_dombes = self.env.ref(
+            "joint_buying_base.supplier_fumet_dombes"
+        )
+        self.partner_supplier_salaison_devidal = self.env.ref(
+            "joint_buying_base.supplier_salaison_devidal"
+        )
+        self.category_all = self.env.ref("product.product_category_all")
 
-    def test(self):
+    def test_search_and_propagate(self):
         len_local_before_local_creation = len(self.ProductProduct.search([]))
         len_joint_buying_before_local_creation = len(
             self.JointBuyingProductProduct.search([])
         )
 
         # Create a new local product
-        self.ProductProduct.create(
+        new_local_product = self.ProductProduct.create(
             {
                 "name": "Some Chocolate",
                 "company_id": self.company_ELD.id,
@@ -34,7 +43,7 @@ class TestModule(TransactionCase):
 
         # Test if the new product is searchable locally
         len_local_after_local_creation = len(self.ProductProduct.search([]))
-        new_local_product = self.assertEqual(
+        self.assertEqual(
             len_local_before_local_creation + 1,
             len_local_after_local_creation,
             "Create a new local product should increase the number of local products",
@@ -63,3 +72,30 @@ class TestModule(TransactionCase):
             "Set a local product as Joint buying should increase the number"
             " of joint buying products",
         )
+
+    def test_joint_buying_product_creation(self):
+        vals = {
+            "name": "Some Product",
+            "categ_id": self.category_all.id,
+            "joint_buying_partner_id": self.company_ELD.joint_buying_partner_id.id,
+            "company_id": False,
+        }
+
+        # create a product for a supplier that is a seller in odoo
+        # should fail
+        with self.assertRaises(ValidationError):
+            self.JointBuyingProductProduct.create(vals)
+
+        # create a product for a supplier that is not a seller in odoo
+        # should success
+        vals.update(
+            {
+                "name": "Some Product 2",
+                "joint_buying_partner_id": self.partner_supplier_fumet_dombes.id,
+            }
+        )
+        product = self.JointBuyingProductProduct.create(vals)
+
+        # Change partner of a joint buying product should fail
+        with self.assertRaises(ValidationError):
+            product.joint_buying_partner_id = self.partner_supplier_salaison_devidal.id
