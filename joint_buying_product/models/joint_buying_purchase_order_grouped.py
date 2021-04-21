185c064fa1a559ec8c9e845d10cc24c9c2792059
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-04-21 15:32:10 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup!  [IMP] joint_buying_product

--- joint_buying_product/models/joint_buying_purchase_order_grouped.py
+++ joint_buying_product/models/joint_buying_purchase_order_grouped.py
@@ -17,15 +17,37 @@ class JointBuyingPurchaseOrderGrouped(models.Model):
         domain="[('is_joint_buying', '=', True), ('supplier', '=', True)]",
     )
 
+    start_date = fields.Date(string="Start Date")
+
+    end_start = fields.Datetime(string="End Date")
+
+    deposit_date = fields.Date(string="Deposit Date")
+
     order_ids = fields.One2many(
         "joint.buying.purchase.order", inverse_name="grouped_order_id"
     )
 
+    order_qty = fields.Integer(
+        string="Orders Quantity", compute="_compute_order_qty", store=True
+    )
+
+    @api.depends("order_ids")
+    def _compute_order_qty(self):
+        for grouped_order in self:
+            grouped_order.order_qty = len(grouped_order.order_ids)
+
     @api.model
-    def _prepare_order_grouped_vals(self, supplier, customers):
+    def _prepare_order_grouped_vals(
+        self, supplier, customers, start_date=False, end_date=False, deposit_date=False
+    ):
         Order = self.env["joint.buying.purchase.order"]
-        vals = {"supplier_id": supplier.id, "order_ids": []}
+        vals = {
+            "supplier_id": supplier.id,
+            "start_date": start_date or supplier.joint_buying_next_start_date,
+            "end_date": end_date or supplier.joint_buying_next_end_date,
+            "deposit_date": deposit_date or supplier.joint_buying_next_deposit_date,
+            "order_ids": [],
+        }
         for customer in customers:
             vals["order_ids"].append((0, 0, Order._prepare_order_vals(customer)))
-
         return vals
