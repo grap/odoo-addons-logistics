0ec8c1b0c3a82b2a7e09e216b37295005d60d889
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-04-23 16:26:53 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup!  [IMP] joint_buying_product

--- joint_buying_base/__manifest__.py
+++ joint_buying_base/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "GRAP,La Jardinière,Hashbang",
     "website": "https://github.com/grap/odoo-addons-logistics/",
     "license": "AGPL-3",
-    "depends": ["base", "mail", "name_search_reset_res_partner"],
+    "depends": ["base", "mail", "name_search_reset_res_partner", "res_company_code"],
     "data": [
         "security/ir_module_category.xml",
         "security/res_groups.xml",
