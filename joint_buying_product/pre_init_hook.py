c148990f83dfc1fe8141b5ef3c075f3869579d72
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-12-10 13:21:36 +0100

    [FIX] joint_buying_product : replace buggy display_joint_buying_propagation (bad stored) by computed on the fly field joint_buying__display_propagation

--- joint_buying_product/pre_init_hook.py
+++ joint_buying_product/pre_init_hook.py
@@ -14,19 +14,6 @@ def pre_init_product_db(cr):
     """
     )
 
-    # Add product_product.display_joint_buying_propagation
-    cr.execute(
-        """
-        ALTER TABLE product_product
-        ADD COLUMN display_joint_buying_propagation boolean;
-        UPDATE product_product pp
-        SET display_joint_buying_propagation = rc.is_joint_buying_customer
-        FROM product_template pt, res_company rc
-        WHERE pt.id = pp.product_tmpl_id
-        AND pt.company_id = rc.id;
-    """
-    )
-
     # Add product_template.is_joint_buying
     cr.execute(
         """
