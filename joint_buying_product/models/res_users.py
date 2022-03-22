67a302df702fb4c728052486b33af6658570fbd2
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-03-22 15:33:41 +0100

    [FIX] joint_buying_xxx : possibility to update company settings from users simple form view

--- joint_buying_product/models/res_users.py
+++ joint_buying_product/models/res_users.py
@@ -9,11 +9,52 @@ class ResUsers(models.Model):
     _inherit = "res.users"
 
     joint_buying_send_pivot_email_in_progress = fields.Boolean(
+        compute="_compute_joint_buying_send_pivot_email_in_progress",
+        inverse="_inverse_joint_buying_send_pivot_email_in_progress",
         string="Send email when opening Grouped Order",
-        related="company_id.joint_buying_send_pivot_email_in_progress",
     )
 
     joint_buying_send_pivot_email_closed = fields.Boolean(
         string="Send email when closing Grouped Order",
-        related="company_id.joint_buying_send_pivot_email_closed",
+        compute="_compute_joint_buying_send_pivot_email_closed",
+        inverse="_inverse_joint_buying_send_pivot_email_closed",
     )
+
+    def __init__(self, pool, cr):
+        """Override of __init__ to add access rights.
+        Access rights are disabled by default, but allowed on some specific
+        fields defined in self.SELF_WRITEABLE_FIELDS.
+        """
+        super().__init__(pool, cr)
+        # duplicate list to avoid modifying the original reference
+        type(self).SELF_WRITEABLE_FIELDS = list(self.SELF_WRITEABLE_FIELDS)
+        type(self).SELF_WRITEABLE_FIELDS.extend(
+            [
+                "joint_buying_send_pivot_email_in_progress",
+                "joint_buying_send_pivot_email_closed",
+            ]
+        )
+
+    def _compute_joint_buying_send_pivot_email_in_progress(self):
+        for user in self:
+            user.joint_buying_send_pivot_email_in_progress = (
+                user.company_id.joint_buying_send_pivot_email_in_progress
+            )
+
+    def _inverse_joint_buying_send_pivot_email_in_progress(self):
+        for user in self:
+            user.company_id.joint_buying_send_pivot_email_in_progress = (
+                user.joint_buying_send_pivot_email_in_progress
+            )
+
+    def _compute_joint_buying_send_pivot_email_closed(self):
+        for user in self:
+            user.joint_buying_send_pivot_email_closed = (
+                user.company_id.joint_buying_send_pivot_email_closed
+            )
+
+    def _inverse_joint_buying_send_pivot_email_closed(self):
+        for user in self:
+            user.company_id.joint_buying_send_pivot_email_closed = (
+                user.joint_buying_send_pivot_email_closed
+            )
