15c1006ecbc5651f6115b2a547647fa348f9d810
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-11-16 15:29:13 +0100

    [IMP] Set a more interesting message error if it's not possible to find a local product from a global product

--- joint_buying_sale/models/joint_buying_purchase_order_line.py
+++ joint_buying_sale/models/joint_buying_purchase_order_line.py
@@ -2,7 +2,8 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import _, models
+from odoo.exceptions import UserError
 
 
 class JointBuyingPurchaseOrderLine(models.Model):
@@ -10,9 +11,17 @@ class JointBuyingPurchaseOrderLine(models.Model):
 
     def _prepare_sale_order_line_vals(self, sale_order):
         self.ensure_one()
+        local_product = self.product_id.get_joint_buying_local_product_id()
+        if not local_product:
+            raise UserError(
+                _(
+                    "Unable to find a local product for the global product"
+                    " '%s'." % (self.product_id.display_name)
+                )
+            )
         return {
             "order_id": sale_order.id,
-            "product_id": self.product_id.get_joint_buying_local_product_id().id,
+            "product_id": local_product.id,
             "product_uom_qty": self.qty,
             "product_uom": self.uom_id.id,
         }
