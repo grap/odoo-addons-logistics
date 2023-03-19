82830b5c41d0cca4e1282705b686441b2ff6345b
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-03-19 22:14:37 +0100

    [IMP] joint_buying_product : add options to show all orders and/or products on the wizard to update grouped orders

--- joint_buying_product/wizards/joint_buying_wizard_update_order_grouped.py
+++ joint_buying_product/wizards/joint_buying_wizard_update_order_grouped.py
@@ -2,12 +2,18 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class JointBuyingWizardUpdateOrderGrouped(models.TransientModel):
     _name = "joint.buying.wizard.update.order.grouped"
     _description = "Joint Buying Wizard Update Order Grouped"
+
+    show_all_orders = fields.Boolean(help="Include empty orders.")
+
+    show_all_products = fields.Boolean(
+        help="Include products that have not been ordered."
+    )
 
     line_ids = fields.Many2many(
         comodel_name="joint.buying.purchase.order.line",
@@ -15,19 +21,33 @@ class JointBuyingWizardUpdateOrderGrouped(models.TransientModel):
         relation="joint_buying_wizard_update_order_grouped_line_rel",
     )
 
+    @api.onchange("show_all_products", "show_all_orders")
+    def onchange_show_settings(self):
+        self.ensure_one()
+        self.line_ids = self._default_line_ids()
+
     def _default_line_ids(self):
         grouped_order = self.env["joint.buying.purchase.order.grouped"].browse(
             self.env.context.get("active_id")
         )
-        ordered_lines = grouped_order.mapped("order_ids.line_ids").filtered(
-            lambda x: x.qty > 0
+        # Compute Ordered Products
+        ordered_product_ids = (
+            grouped_order.mapped("order_ids.line_ids")
+            .filtered(lambda x: x.qty > 0)
+            .mapped("product_id")
+            .ids
         )
-        ordered_product_ids = ordered_lines.mapped("product_id").ids
-        not_null_orders = grouped_order.mapped("order_ids").filtered(
-            lambda x: x.amount_untaxed > 0
+
+        # Compute Not Null Orders
+        not_null_order_ids = (
+            grouped_order.mapped("order_ids")
+            .filtered(lambda x: x.amount_untaxed > 0)
+            .ids
         )
-        return not_null_orders.mapped("line_ids").filtered(
-            lambda x: x.product_id.id in ordered_product_ids
+
+        return grouped_order.mapped("order_ids.line_ids").filtered(
+            lambda x: (self.show_all_orders or x.order_id.id in not_null_order_ids)
+            and (self.show_all_products or x.product_id.id in ordered_product_ids)
         )
 
     def save_close(self):
