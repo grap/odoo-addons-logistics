93a5df15b3f8c7c5403bacc1511e540122f27144
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-07-27 16:39:41 +0200

    [REF] joint_buying_base : refactor check.access.mixin for better readability

--- joint_buying_base/models/joint_buying_check_access_mixin.py
+++ joint_buying_base/models/joint_buying_check_access_mixin.py
@@ -17,69 +17,89 @@ class JointBuyingCheckAccessMixin(models.AbstractModel):
     @api.model
     def create(self, vals):
         res = super().create(vals)
-        if not self.env.user.has_group(
-            "joint_buying_base.group_joint_buying_manager"
-        ) and not self.env.context.get("no_check_joint_buying", False):
-            if (
-                "is_joint_buying" not in self._fields or res.is_joint_buying
-            ) and res.mapped(
-                self._check_write_access_company_field_id
-            ) != self.env.user.company_id:
-                raise AccessError(
-                    _(
-                        "You can not create this item because"
-                        " you are not responsible for it"
-                    )
+        if self.env.context.get("no_check_joint_buying", False):
+            # explicitely ignore the check, if asked
+            return res
+
+        if self.env.user.has_group("joint_buying_base.group_joint_buying_manager"):
+            # A manager has no contrains
+            return res
+
+        if "is_joint_buying" in self._fields and not res.is_joint_buying:
+            # if it's a mixed model (global / local) like product and partner
+            # and if we are in a local context, no check.
+            return res
+
+        if (
+            res.mapped(self._check_write_access_company_field_id)
+            != self.env.user.company_id
+        ):
+            raise AccessError(
+                _(
+                    "You can not create this item because"
+                    " you are not responsible for it"
                 )
+            )
         return res
 
     @api.multi
     def write(self, vals):
-        to_check_fields = [
-            x
-            for x in list(vals.keys())
-            if x not in self._check_write_access_fields_no_check
-        ]
-        if (
-            not self.env.user.has_group("joint_buying_base.group_joint_buying_manager")
-            and not self.env.context.get("no_check_joint_buying", False)
-            and to_check_fields
-        ):
-            if "is_joint_buying" in self._fields:
-                items = self.filtered(lambda x: x.is_joint_buying)
-            else:
-                items = self
-            for item in items:
-                if (
-                    item.mapped(self._check_write_access_company_field_id)
-                    != self.env.user.company_id
-                ):
-                    raise AccessError(
-                        _(
-                            "You can not update this item because"
-                            " you are not responsible for it"
-                        )
+        if self.env.context.get("no_check_joint_buying", False):
+            # explicitely ignore the check, if asked
+            return super().write(vals)
+
+        if self.env.user.has_group("joint_buying_base.group_joint_buying_manager"):
+            # A manager has no contrains
+            return super().write(vals)
+
+        if not (set(vals.keys()) - set(self._check_write_access_fields_no_check)):
+            # Updated fields are all unprotected
+            return super().write(vals)
+
+        # Filter on joint buying items
+        if "is_joint_buying" in self._fields:
+            items = self.filtered(lambda x: x.is_joint_buying)
+        else:
+            items = self
+
+        for item in items:
+            if (
+                item.mapped(self._check_write_access_company_field_id)
+                != self.env.user.company_id
+            ):
+                raise AccessError(
+                    _(
+                        "You can not update this item because"
+                        " you are not responsible for it"
                     )
+                )
         return super().write(vals)
 
     @api.multi
     def unlink(self):
-        if not self.env.user.has_group(
-            "joint_buying_base.group_joint_buying_manager"
-        ) and not self.env.context.get("no_check_joint_buying", False):
-            if "is_joint_buying" in self._fields:
-                items = self.filtered(lambda x: x.is_joint_buying)
-            else:
-                items = self
-            for item in items:
-                if (
-                    item.mapped(self._check_write_access_company_field_id)
-                    != self.env.user.company_id
-                ):
-                    raise AccessError(
-                        _(
-                            "You can not unlink this item because"
-                            " you are not responsible for it"
-                        )
+        if self.env.context.get("no_check_joint_buying", False):
+            # explicitely ignore the check, if asked
+            return super().unlink()
+
+        if self.env.user.has_group("joint_buying_base.group_joint_buying_manager"):
+            # A manager has no contrains
+            return super().unlink()
+
+        # Filter on joint buying items
+        if "is_joint_buying" in self._fields:
+            items = self.filtered(lambda x: x.is_joint_buying)
+        else:
+            items = self
+
+        for item in items:
+            if (
+                item.mapped(self._check_write_access_company_field_id)
+                != self.env.user.company_id
+            ):
+                raise AccessError(
+                    _(
+                        "You can not unlink this item because"
+                        " you are not responsible for it"
                     )
+                )
         return super().unlink()
