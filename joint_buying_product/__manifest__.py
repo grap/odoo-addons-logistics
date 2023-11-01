fe2278bdd19b570edf725f37979d0fa6f5c08eff
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-11-01 15:12:14 +0100

    [ADD] joint_buying_product : New view for joint.buying.transport.request.line

--- joint_buying_product/__manifest__.py
+++ joint_buying_product/__manifest__.py
@@ -37,6 +37,7 @@
         "views/view_joint_buying_purchase_order_grouped.xml",
         "views/view_joint_buying_purchase_order.xml",
         "views/view_joint_buying_transport_request.xml",
+        "views/view_joint_buying_transport_request_line.xml",
         "views/view_res_partner.xml",
         "reports/report_joint_buying_purchase_order_grouped.xml",
         "reports/report.xml",
