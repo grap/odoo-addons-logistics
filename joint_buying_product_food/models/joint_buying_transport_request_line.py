915b4d91ecd0fc5d8268ae99ea2aaba8f896e59c
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-11-22 23:19:24 +0100

    [REF] refactor tour report. change order [REF] refactor joint.buying.transport.request : move into joint_buying_base module

--- joint_buying_product_food/models/joint_buying_transport_request_line.py
+++ joint_buying_product_food/models/joint_buying_transport_request_line.py
@@ -8,23 +8,23 @@ from odoo import models
 class JointBuyingTransportRequestLine(models.Model):
     _inherit = "joint.buying.transport.request.line"
 
-    def get_report_information(self):
-        self.ensure_one()
-        res = super().get_report_information()
-        if self.request_id.order_id:
-            return {
-                "normal": [
-                    x for x in filter(lambda x: x[0].storage_method, res["all"])
-                ],
-                "cool": [
-                    x
-                    for x in filter(lambda x: x[0].storage_method == "cool", res["all"])
-                ],
-                "frozen": [
-                    x
-                    for x in filter(
-                        lambda x: x[0].storage_method == "frozen", res["all"]
-                    )
-                ],
-            }
-        return res
+    # def get_report_information(self):
+    #     self.ensure_one()
+    #     res = super().get_report_information()
+    #     if self.request_id.order_id:
+    #         return {
+    #             "normal": [
+    #                 x for x in filter(lambda x: x[0].storage_method, res["all"])
+    #             ],
+    #             "cool": [
+    #                 x
+    #                 for x in filter(lambda x: x[0].storage_method == "cool", res["all"])
+    #             ],
+    #             "frozen": [
+    #                 x
+    #                 for x in filter(
+    #                     lambda x: x[0].storage_method == "frozen", res["all"]
+    #                 )
+    #             ],
+    #         }
+    #     return res
