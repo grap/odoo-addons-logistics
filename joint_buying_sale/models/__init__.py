3772fb6980ae40d3514b2802d066ce6ad92fe27b
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-12-02 19:36:40 +0100

    [REF] joint_buying_* : refactor tour report. allow to split request into many lines, because of different categories

--- joint_buying_sale/models/__init__.py
+++ joint_buying_sale/models/__init__.py
@@ -2,3 +2,4 @@ from . import joint_buying_purchase_order
 from . import joint_buying_purchase_order_line
 from . import joint_buying_transport_request
 from . import sale_order
+from . import sale_order_line
