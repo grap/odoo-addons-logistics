d93ab9f4e4757b76f2af6a9aefd3f30dd6b466f9
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-01-22 16:57:49 +0100

    [REM] joint_buying_base : cost_chart. (+ pandas + bokeh dependency)

--- setup/joint_buying_base/setup.py
+++ setup/joint_buying_base/setup.py
@@ -2,11 +2,5 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon={
-        'external_dependencies_override': {
-            'python': {
-                'bokeh': 'bokeh==1.1.0',
-            }
-        }
-    },
+    odoo_addon=True,
 )
