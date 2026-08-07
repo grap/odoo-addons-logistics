eb250ce936774bec623a6088c5b43fa7c7a36c11
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-08-07 16:42:10 +0200

    [MIG] joint_buying_product_label: Migration to 16.0 (from 12.0)

--- joint_buying_product_label/__manifest__.py
+++ joint_buying_product_label/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Joint Buying - Product Labels",
-    "version": "12.0.1.0.3",
+    "version": "16.0.1.0.0",
     "category": "GRAP - Logistics",
     "author": "GRAP",
     "website": "https://github.com/grap/odoo-addons-logistics",
